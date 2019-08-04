51a70a230956e1139e0d5a6dcd457b8d6165d8bd
lloyds <parsfan@hotmail.com>
2019-08-04 17:31:21 +0100

    Tinkering with network settings, Running Builds in Debug mode

--- create_docker_images.sh
+++ create_docker_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build -t purplefridaytweetlistener:latest -f PurpleFridayTweetListener/Dockerfile PurpleFridayTweetListener/
-docker build -t purplefriday_wa:latest -f Web.Admin/Dockerfile Web.Admin
+docker build -t purplefriday_wa:latest -f Web.Admin/Dockerfile Web.Admin/
