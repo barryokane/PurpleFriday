29676419e534ffdf196aabd2c7d3a1175efca5a7
Hugh Aristotle Powell <hugh@swirrl.com>
2019-08-04 13:04:22 +0100

    Create user and use nginx for deployment

--- deployment/run.sh
+++ deployment/run.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
+set -e
+
 user=$1
 ip_address=$2
 cd $3
 root_dir=`pwd`
 
 echo $root_dir
+
+cd $root_dir
+git checkout master
+git pull
 
 cd $root_dir/Web.Admin
 
@@ -27,6 +33,6 @@ zip -r $root_dir/deployment/PurpleFridayTweetListener.zip .
 
 cd $root_dir/deployment
 
-scp PurpleFridayWebApp.zip PurpleFridayTweetListener.zip remote-deployment.sh PurpleFridayWebApp.service PurpleFridayTweetListener.service $user@$ip_address:~/
+scp PurpleFridayWebApp.zip PurpleFridayTweetListener.zip remote-deployment.sh PurpleFridayWebApp.service PurpleFridayTweetListener.service purple-friday-http $user@$ip_address:~/
 
 ssh $user@$ip_address -t './remote-deployment.sh'
